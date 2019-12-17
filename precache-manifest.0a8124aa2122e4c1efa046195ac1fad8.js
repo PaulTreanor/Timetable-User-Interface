2b83ea34dab68210330a068ba60f737cafad6e75
Paul Treanor <treanorpaul9@gmail.com>
2019-12-17 19:19:13 +0000

    Updates

---
+++ precache-manifest.0a8124aa2122e4c1efa046195ac1fad8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4994ce0f9fabad62888961eb4c5ed098",
+    "url": "/ui_func/index.html"
+  },
+  {
+    "revision": "f882be566eb47e6844cd",
+    "url": "/ui_func/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2f20efd173564fb82b86",
+    "url": "/ui_func/static/css/main.17fdaf95.chunk.css"
+  },
+  {
+    "revision": "f882be566eb47e6844cd",
+    "url": "/ui_func/static/js/2.4c881543.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/ui_func/static/js/2.4c881543.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f20efd173564fb82b86",
+    "url": "/ui_func/static/js/main.5610eebc.chunk.js"
+  },
+  {
+    "revision": "8bd177612bd0809dd8ae",
+    "url": "/ui_func/static/js/runtime-main.18bcc38e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clubs_socs_ui_timetable/precache-manifest.b452067eb1f8143690b78b26bbab1006.js"
+  "/ui_func/precache-manifest.0a8124aa2122e4c1efa046195ac1fad8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clubs_socs_ui_timetable/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ui_func/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

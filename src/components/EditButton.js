22387c4cec5280e92dcf05bb3735f21bd79a4dac
Michael S <33690140+michaelssavage@users.noreply.github.com>
2019-12-18 00:25:23 +0000

    Update EditButton.js
    Added plus sign

--- src/components/EditButton.js
+++ src/components/EditButton.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const EditButton = props => {
-    return <button class="btn btn-primary mb-3" type="button" onClick={props.addTrip}>Edit/Add Event</button>
+    return <button class="btn btn-primary mb-3" type="button" onClick={props.addTrip}>
+      Edit/Add Event <span>&#43;</span>
+    </button>
   }
 
 export default EditButton;

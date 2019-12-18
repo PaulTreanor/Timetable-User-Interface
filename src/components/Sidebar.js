3d33f49a289173d2810796e382c004e5d7b60f2a
Michael S <33690140+michaelssavage@users.noreply.github.com>
2019-12-18 00:29:20 +0000

    Update Sidebar.js
    formatting and uppercase Club/Society

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,20 +11,18 @@ class Sidebar extends React.Component {
         <div class="col-md-3 p-0 m-0">
             <div class= "sidebar border-dark ">
 
-
               <form class="form-inline my-2 my-lg-0">
-
                 <div class="searchy">
-                  <h6>Search for club/society</h6>
+                  <h6>Search for Club/Society</h6>
                   <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-                  <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+                  <button class="btn btn-primary my-2 my-sm-0" type="submit">
+                    <i class = "fa fa-search"></i>
+                  </button>
                 </div>
               </form>
-
+              
             </div>
           </div>
-        
-  
     );
   }
 }
